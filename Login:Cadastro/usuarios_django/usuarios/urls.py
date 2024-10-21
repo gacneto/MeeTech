f2046cc22c7360b67de5c938f6f1cbc234a11757
Lucas Barcelar <163942203+gacneto@users.noreply.github.com>
2024-10-21 09:31:26 -0300

    Delete Login:Cadastro/usuarios_django directory

--- Login:Cadastro/usuarios_django/usuarios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('login/', views.login_usuario, name='login'),
-]
